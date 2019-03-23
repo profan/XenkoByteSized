e61e3d6bd256f7e4d79d8353c8f0b4be4ff0f406
Robin Hübner <robinhubner@gmail.com>
2019-03-23 23:44:09 +0100

    commit beginning of multimesh, very borked at the moment however.

--- XenkoByteSized/ProceduralMesh/CombinedMeshes.cs
+++ XenkoByteSized/ProceduralMesh/CombinedMeshes.cs
@@ -3,108 +3,117 @@ using Xenko.Core.Collections;
 using Xenko.Engine;
 using Xenko.Rendering;
 using Xenko.Graphics;
+using Xenko.Graphics.GeometricPrimitives;
+using Xenko.Extensions;
 
 namespace XenkoByteSized.ProceduralMesh {
 
     class PoorMansMultiMesh : SyncScript {
 
+        const int INITIAL_INSTANCE_COUNT = 16;
+
         private TrackingDictionary<System.Guid, TransformComponent> transforms = new TrackingDictionary<System.Guid, TransformComponent>();
-        
-        /* gpu side data */
+        private FastList<Matrix> matrices = new FastList<Matrix>();
+
+        /* gpu pipeline stuff */
+        private PipelineState pipelineState;
+
+        /* gpu side data, our ModelComponent we use to render, no index buffer data so there is some... waste yes */
         private ModelComponent modelComponent;
-        private VertexBufferBinding vertexBinding;
-        private IndexBufferBinding indexBinding;
+        private VertexBufferBinding streamOutBufferBinding;
+        private EffectInstance streamShader;
+        private Mesh streamBufferedMesh;
 
-        public Material[] Materials;
-        public Mesh Mesh;
+        private Mesh renderedMesh;
+        public Mesh Mesh {
+            get {
+                return renderedMesh;
+            }
+            set {
+                renderedMesh = value;
+            }
+        }
 
-        System.Guid AddInstance(TransformComponent transform) {
+        public System.Guid AddInstance(TransformComponent transform) {
             transforms[transform.Id] = transform;
             return transform.Id;
         }
 
-        bool RemoveInstance(TransformComponent transform) {
+        public bool RemoveInstance(TransformComponent transform) {
             return transforms.Remove(transform.Id);
         }
 
-        void CheckBuffers() {
+        private void CreateDeviceObjects() {
+
+            var commandList = Game.GraphicsContext.CommandList;
+
+            var shader = new EffectInstance(EffectSystem.LoadEffect("MultiMeshShader").WaitForResult());
+            streamShader = shader;
+
+            var pipeline = new PipelineStateDescription() {
+                // BlendState = BlendStates.Default,
+                // RasterizerState = RasterizerStateDescription.Default,
+                // DepthStencilState = DepthStencilStates.None,
+                // Output = new RenderOutputDescription(GraphicsDevice.Presenter.BackBuffer.Format),
+                PrimitiveType = PrimitiveType.TriangleList,
+                InputElements = VertexPositionNormalTexture.Layout.CreateInputElements(),
+                EffectBytecode = shader.Effect.Bytecode,
+                RootSignature = shader.RootSignature,
+            };
+
+            var newPipelineState = PipelineState.New(GraphicsDevice, ref pipeline);
+            pipelineState = newPipelineState;
+
+            var streamBuffer = Buffer.New<VertexPositionNormalTexture>(GraphicsDevice, INITIAL_INSTANCE_COUNT, BufferFlags.StreamOutput);
+            streamOutBufferBinding = new VertexBufferBinding(streamBuffer, VertexPositionNormalTexture.Layout, streamBuffer.ElementCount);
+
+        }
+
+        private void CheckBuffers() {
 
             var device = GraphicsDevice;
-            var layout = Mesh.Draw.VertexBuffers[0].Declaration;
 
-            var vbo = Mesh.Draw.VertexBuffers[0].Buffer;
-            var vboInBytes = vbo.SizeInBytes;
-
-            var ibo = Mesh.Draw.IndexBuffer.Buffer;
-            var iboInBytes = ibo.SizeInBytes;
-
-            uint neededVboSize = (uint)(transforms.Count * vboInBytes);
-            uint neededIboSize = (uint)(iboInBytes);
-
-            if (neededVboSize > vertexBinding.Buffer.SizeInBytes) {
-                var vertexBuffer = Buffer.Vertex.New(device, (int)(neededVboSize * 1.5f));
-                vertexBinding = new VertexBufferBinding(vertexBuffer, layout, 0);
-            }
-
-            if (neededIboSize > indexBinding.Buffer.SizeInBytes) {
-                var is32Bits = false;
-                var indexBuffer = Buffer.Index.New(device, (int)(neededIboSize * 1.5f));
-                indexBinding = new IndexBufferBinding(indexBuffer, is32Bits, 0);
+            uint neededBufferSize = (uint)(transforms.Count);
+            if (neededBufferSize > streamOutBufferBinding.Count) {
+                streamOutBufferBinding.Buffer.Dispose(); // dispose the old buffer first
+                var streamBuffer = Buffer.New<VertexPositionNormalTexture>(device, (int)(neededBufferSize), BufferFlags.StreamOutput);
+                streamOutBufferBinding = new VertexBufferBinding(streamBuffer, VertexPositionNormalTexture.Layout, streamBuffer.ElementCount);
             }
 
         }
 
-        void UpdateBuffers() {
+        private void PerformStreamOut() {
 
-            for (int i = 0; i < transforms.Count; ++i) {
+            var commandList = Game.GraphicsContext.CommandList;
 
-            }
+            /* TODO: this currently assumes a single vertex buffer, is this always the case? */
+            var vertexBuffer = renderedMesh.Draw.VertexBuffers[0].Buffer;
+            var indexBuffer = renderedMesh.Draw.IndexBuffer.Buffer;
+
+            commandList.SetPipelineState(pipelineState);
+
+            /* TODO: this currently assumes a single vertex buffer, is this always the case? */
+            commandList.SetVertexBuffer(0, vertexBuffer, 0, VertexPositionNormalTexture.Layout.VertexStride);
+            commandList.SetIndexBuffer(indexBuffer, 0, is32bits: true);
+            commandList.SetStreamTargets(streamOutBufferBinding.Buffer);
+            
+            // streamShader.Parameters.Set(MultiMeshShaderKeys.modelTransforms, matrices.Items);
+            streamShader.UpdateEffect(GraphicsDevice);
+
+            /* finally write to our streamout buffer */
+            commandList.DrawIndexedInstanced(vertexBuffer.ElementCount, transforms.Count);
 
         }
 
-        public override void Start() {
-
-        }
-
-        public override void Update() {
-
-        }
-
-        public override void Cancel() {
-
-        }
-
-    }
-
-    public class CombinedMeshes : SyncScript {
-
-        /* GPU side data */
-        private ModelComponent modelComponent;
-        private Mesh mesh;
-
-        /* rotation in radians per second */
-        public float rotationSpeed = MathUtil.PiOverFour;
-
-        static private Mesh CreateMesh(GraphicsDevice device, VertexPositionNormalTexture[] verts, uint[] indices) {
-
-            /* now set up the GPU side stuff */
-            var vbo = Xenko.Graphics.Buffer.Vertex.New(
-                device,
-                verts, /* allocated size of buffer inferred from the stored datatype and the length of the array */
-                GraphicsResourceUsage.Default /* usage hint to the GPU for it to allocate it appropriately (explicit default in our case) */
-            );
-
-            /* NOTE: if resource usage here  is set to immutable (the default) you will encounter an error if you try to update it after creating it */
-            var ibo = Xenko.Graphics.Buffer.Index.New(device, indices, GraphicsResourceUsage.Default);
-
+        static private Mesh CreateMesh(VertexBufferBinding bufferBinding) {
+            
             var newMesh = new Mesh() {
                 Draw = new MeshDraw() {
                     PrimitiveType = PrimitiveType.TriangleList,
                     VertexBuffers = new[] {
-                        new VertexBufferBinding(vbo, VertexPositionNormalTexture.Layout, verts.Length),
+                        bufferBinding,
                     },
-                    IndexBuffer = new IndexBufferBinding(ibo, is32Bit: true, count: indices.Length),
-                    DrawCount = indices.Length
+                    DrawCount = bufferBinding.Count
                 }
             };
 
@@ -114,19 +123,94 @@ namespace XenkoByteSized.ProceduralMesh {
 
         public override void Start() {
 
-            var vertices = new VertexPositionNormalTexture[0];
-            var indices = new uint[0];
+            CreateDeviceObjects();
 
-            mesh = CreateMesh(GraphicsDevice, vertices, indices);
+            streamBufferedMesh = CreateMesh(streamOutBufferBinding);
 
-            /* create our ModelComponent and add the mesh to it */
             modelComponent = new ModelComponent() {
                 Model = new Model() {
-                    mesh,
+                    streamBufferedMesh
                 }
             };
 
             Entity.Add(modelComponent);
+
+        }
+                
+        private void UpdateBuffers() {
+
+            CheckBuffers();
+
+            /* update our MeshDraw to reflect current state */
+            var vertexCount = streamOutBufferBinding.Count;
+            streamBufferedMesh.Draw.VertexBuffers[0] = streamOutBufferBinding;
+            streamBufferedMesh.Draw.DrawCount = vertexCount;
+
+        }
+
+        private void UpdateMatrices() {
+
+            matrices.Clear();
+            foreach (var transform in transforms) {
+                matrices.Add(transform.Value.LocalMatrix);
+            }
+
+        }
+
+        public override void Update() {
+
+            UpdateBuffers();
+            UpdateMatrices();
+            PerformStreamOut();
+
+        }
+
+        public override void Cancel() {
+            streamOutBufferBinding.Buffer.Dispose();
+            streamShader.Dispose();
+        }
+
+    }
+
+    class SomeObjectInSpace : SyncScript {
+
+        public int Id;
+
+        public override void Update() {
+            var worldPos = Entity.Transform.WorldMatrix.TranslationVector;
+            DebugText.Print($"{Id} - Position - x: {worldPos.X}, y: {worldPos.Y}", new Int2(16, 16 * Id));
+        }
+
+    }
+
+    public class CombinedMeshes : SyncScript {
+
+        /* GPU side data */
+        private PoorMansMultiMesh multiMesh;
+
+        /* rotation in radians per second */
+        public float rotationSpeed = MathUtil.PiOverFour;
+
+        public override void Start() {
+
+            var spherePrimitive = GeometricPrimitive.Sphere.New(GraphicsDevice, 0.5f);
+            var sphereMeshDraw = spherePrimitive.ToMeshDraw();
+
+            var newMultiMesh = new PoorMansMultiMesh() {
+                Mesh = new Mesh() {
+                    Draw = sphereMeshDraw
+                }
+            };
+
+            var numInstances = 5;
+            for (int i = 0; i < numInstances; ++i) {
+                var newObjectInSpace = new SomeObjectInSpace() { Id = i };
+                newMultiMesh.AddInstance(Entity.Transform);
+                Entity.Add(newObjectInSpace);
+            }
+
+            Entity.Add(newMultiMesh);
+            multiMesh = newMultiMesh;
 
         }
 
@@ -136,12 +220,6 @@ namespace XenkoByteSized.ProceduralMesh {
             var rotation = Quaternion.RotationY(rotationSpeed * deltaTime);
             Entity.Transform.Rotation *= rotation;
 
-        }
-
-        // FIXME: can this be done differently? its not a hack but..
-        public override void Cancel() {
-            mesh.Draw.VertexBuffers[0].Buffer.Dispose();
-            mesh.Draw.IndexBuffer.Buffer.Dispose();
         }
 
     }
