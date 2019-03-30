2a57abf0c592e2da7088003c7d9506915922d3ec
Robin Hübner <robinhubner@gmail.com>
2019-03-31 00:25:34 +0100

    all of this is probably way off (nonfunctional still, but progres maybe?

---
+++ XenkoByteSized/ProceduralMesh/MaterialVertexFeature.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xenko.Core;
+using Xenko.Core.Annotations;
+using Xenko.Rendering.Materials;
+using Xenko.Rendering.Materials.ComputeColors;
+using Xenko.Shaders;
+
+namespace XenkoByteSized.ProceduralMesh {
+
+    class OurMaterialKeys {
+
+    }
+
+    [DataContract("MaterialVertexFeature")]
+    [Display("Vertex Shader")]
+    public class MaterialVertexFeature : MaterialFeature, IMaterialDisplacementFeature {
+
+        public const string OurVertexStream = "OurPosition";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MaterialVertexFeature"/> class.
+        /// </summary>
+        public MaterialVertexFeature() : this(new ComputeColor())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MaterialVertexFeature"/> class.
+        /// </summary>
+        /// <param name="displacementMap">The displacement map.</param>
+        public MaterialVertexFeature(IComputeColor vertexShader) {
+            VertexShader = vertexShader;
+            Stage = DisplacementMapStage.Vertex;
+        }
+
+        /// <summary>
+        /// Gets or sets the displacement map.
+        /// </summary>
+        /// <value>The displacement map.</value>
+        /// <userdoc>
+        /// The map containing the displacement offsets to apply onto the model vertex positions.
+        /// </userdoc>
+        [DataMember(10)]
+        [Display("Vertex Shader")]
+        [NotNull]
+        public IComputeColor VertexShader { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating in which stage the displacement should occur.
+        /// </summary>
+        /// <userdoc>
+        /// The value indicating in which stage the displacement will occur.
+        /// </userdoc>
+        [DataMember(40)]
+        [DefaultValue(true)]
+        [Display("Shader Stage")]
+        public DisplacementMapStage Stage { get; set; }
+
+        public override void GenerateShader(MaterialGeneratorContext context) {
+
+            if (VertexShader == null) { return; }
+
+            var materialStage = (MaterialShaderStage)Stage;
+
+            // reset the displacement streams at the beginning of the stage
+            context.AddStreamInitializer(materialStage, "OurVertexStream");
+
+            // use... (FIXME: is this necessary? why?)
+            context.UseStream(materialStage, OurVertexStream);
+
+            // build the vertex shader
+            var vertexShader = VertexShader;
+
+            // Workaround to inform compute colors that sampling is occurring from a vertex shader
+            context.IsNotPixelStage = materialStage != MaterialShaderStage.Pixel;
+            context.SetStream(materialStage, OurVertexStream, vertexShader, MaterialKeys.DisplacementMap, MaterialKeys.DisplacementValue);
+            context.IsNotPixelStage = false;
+
+            // var scaleNormal = materialStage != MaterialShaderStage.Vertex;
+            var positionMember = materialStage == MaterialShaderStage.Vertex ? "Position" : "PositionWS";
+            var normalMember = materialStage == MaterialShaderStage.Vertex ? "meshNormal" : "normalWS";
+            context.SetStreamFinalModifier<MaterialVertexFeature>(materialStage, new ShaderClassSource("MaterialVertexDisplacement", positionMember, normalMember));
+
+        }
+
+    }
+}
---
+++ XenkoByteSized/ProceduralMesh/WavyPlaneDisplacement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xenko.Core.Mathematics;
+using Xenko.Input;
+using Xenko.Engine;
+using Xenko.Rendering;
+using Xenko.Rendering.Materials;
+using Xenko.Graphics;
+using Xenko.Rendering.Materials.ComputeColors;
+using XenkoByteSized.ProceduralMesh;
+
+namespace XenkoByteSized {
+    public class WavyPlaneDisplacement : StartupScript {
+
+        private ModelComponent modelComponent;
+        private EffectInstance wavyShader;
+
+        public override void Start() {
+
+            var newWavyShader = new EffectInstance(EffectSystem.LoadEffect("WavyPlaneVertices").WaitForResult());
+            newWavyShader.UpdateEffect(GraphicsDevice);
+            wavyShader = newWavyShader;
+
+            Material newVertexColorMaterial = Material.New(GraphicsDevice, new MaterialDescriptor {
+                Attributes = new MaterialAttributes {
+                    Displacement = new MaterialVertexFeature(),
+                    Diffuse = new MaterialDiffuseMapFeature(new ComputeVertexStreamColor()),
+                    DiffuseModel = new MaterialDiffuseLambertModelFeature(),
+                    CullMode = CullMode.None,
+                },
+            });
+
+            modelComponent = Entity.Get<ModelComponent>();
+            modelComponent.Model.Add(newVertexColorMaterial);
+
+        }
+
+        public override void Cancel() {
+            wavyShader.Dispose();
+        }
+
+    }
+}
